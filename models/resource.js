6fc71c651cb0d0064ddf28c1bd5c18b7d1762553
José Carneiro <contact@josecarnei.ro>
2022-06-04 13:57:56 +0100

    Add timestamps

--- models/resource.js
+++ models/resource.js
@@ -2,21 +2,24 @@
 
 const mongoose = require('mongoose');
 
-const schema = new mongoose.Schema({
-  block: {
-    type: String,
-    required: true,
-    trim: true,
-    lowercase: true,
-    minlength: 1,
-    maxlength: 40
+const schema = new mongoose.Schema(
+  {
+    block: {
+      type: String,
+      required: true,
+      trim: true,
+      lowercase: true,
+      minlength: 1,
+      maxlength: 40
+    },
+    data: {
+      type: mongoose.Schema.Types.Mixed,
+      required: true,
+      default: () => ({})
+    }
   },
-  data: {
-    type: mongoose.Schema.Types.Mixed,
-    required: true,
-    default: () => ({})
-  }
-});
+  { timestamps: true }
+);
 
 const Resource = mongoose.model('Resource', schema);
 
